5da8e955e5693adbb06eb69402cf67c23ef854da
kalratanav <kalratanav@gmail.com>
2023-01-28 14:39:59 -0600

    Created data entry

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+  const [floors, setFloors] = useState("");
+  const [teams, setTeams] = useState("");
+  const [selectedFile, setSelectedFile] = useState("");
+  const [isFilePicked, setIsFilePicked] = useState("");
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+  };
+
+  const handleSubmission = () => {
+
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='Form'>
+          <form>
+            <label>Enter number of floors: &emsp;
+              <input 
+                type="text"
+                value = {floors}
+                onChange={(e) => setFloors(e.target.value)}
+              />
+            </label>
+          </form>
+          <form>
+            <label>Enter number of teams: &emsp;
+              <input 
+                type="text" 
+                value = {teams}
+                onChange={(e) => setTeams(e.target.value)}
+              />
+            </label>
+          </form>
+          <label>
+            Please upload teams .csv file: 
+          </label>
+          <div>
+            <input 
+              type="file"
+              name="file"
+              onChange={changeHandler}
+            />
+            <div>
+              <button onClick={handleSubmission}>Submit</button>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
 }
+
 
 export default App;
