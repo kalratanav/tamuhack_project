660d67aeda6534d469b05979d2ba387ca558cc0e
Ren <117477886+maiqlan@users.noreply.github.com>
2023-01-28 16:02:01 -0600

    floor header

--- floor.cpp
+++ floor.cpp
@@ -1,15 +1,18 @@
 #include "floor.h"
 
 void floor::change_occupied(){
+    //updating the occupied value
+    double sum 0;
+    for (unsigned int = 0; i < teams.size()){
+        sum += teams.at(0).team_number;
+    }
 
-
+    occupied = sum/(static_cast<double>(capacity));
 }
 
 void floor::add_team(Team filler){
-
-
+ teams.push_back(filler);
 }
-
 
 floor::floor(/* args */)
 {
@@ -20,9 +23,8 @@ floor::~floor()
 }
 
 
-void floor::change_bool(){
-
-
+void floor::change_bool(bool new){
+    seventy_five_populated = new;
 }
 
 
