6f26209ea868297ca97c4f3b37bbd3a93a6b9536
Ren <117477886+maiqlan@users.noreply.github.com>
2023-01-28 12:59:24 -0600

    Add files via upload

---
+++ floor.h
@@ -0,0 +1,40 @@
+#ifndef FLOOR_H
+#define FLOOR_H
+
+#include <vector>
+#include <string>
+
+using std::vector;
+#including "team.h"
+
+class floor
+{
+private:
+    vector <Team> teams; //each floor will contain a vector of teams that are on the floor
+    bool seventy_five_populated = false;
+    unsigned int capacity; //this value cannot be changed
+    unsigned int occupied; // this will be the sizes of the teams divided by the capacity
+public:
+    floor(); 
+    ~floor();
+    void change_occupied(); //will be updated
+    void add_team(Team filler); // add to the vector
+    //change bool...if occupied/capacity > .75, then, we can change the bool val
+    void change_bool();
+
+
+
+
+};
+
+floor::floor(/* args */)
+{
+}
+
+floor::~floor()
+{
+}
+
+
+#include <vector>
+#include <string>
