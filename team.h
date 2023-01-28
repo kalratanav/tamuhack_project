c90428ebb2acdd82c1d234a627e722a82fd6fa0f
Ren <117477886+maiqlan@users.noreply.github.com>
2023-01-28 11:40:52 -0600

    Add files via upload
    Temporary c++ header file for team class, may implement methods of comparison and check floor occupation?

---
+++ team.h
@@ -0,0 +1,26 @@
+#ifndef TEAM_H
+#define TEAM_H
+
+class team
+{
+private:
+    /* data */
+public:
+    team(/* args */);
+    ~team();
+
+    
+};
+
+team::team(/* args */)
+{
+
+}
+
+team::~team()
+{
+
+}
+
+
+#endif
