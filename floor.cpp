142b1593cb97300f368fc61f3d529793e29a1253
Ren <117477886+maiqlan@users.noreply.github.com>
2023-01-28 15:37:52 -0600

    methods

--- floor.cpp
+++ floor.cpp
@@ -1,2 +1,28 @@
 #include "floor.h"
 
+void floor::change_occupied(){
+
+
+}
+
+void floor::add_team(Team filler){
+
+
+}
+
+
+floor::floor(/* args */)
+{
+}
+
+floor::~floor()
+{
+}
+
+
+void floor::change_bool(){
+
+
+}
+
+
