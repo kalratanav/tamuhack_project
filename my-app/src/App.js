aa261d16cd3a257b2662cc9f16a3c92d23822af9
kalratanav <kalratanav@gmail.com>
2023-01-28 23:33:09 -0600

    more

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,13 @@ function App() {
   const [file1, setFile1] = useState(null);
   const [file2, setFile2] = useState(null);
 
+  const handleClickScroll= () => {
+    const element = document.getElementById('section2');
+    if (element) {
+      element.scrollIntoView({behavior: "smooth"});
+    }
+  };
+
   const handleSubmission = (e) => {
       
       try {
@@ -26,55 +33,65 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='topLine'>
-          <div className='titleContainer'>
-            <h1 className='title'>
-              <span>
-                <a className = 'titleLink' href="index.html">Space-Probe</a>
-                </span>
-            </h1>
-          </div>
-          <div className="navbar">
-            <nav>
-              <ul>
-                <li className="aboutLink">
-                  About
-                </li>
-                <li className='loginLink'> 
-                  Login
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-        <div className='inputContainer'>
-          <div className='box'>
-          <div className='Form'>
-            <label className='PromptLabel'>
-              Upload teams.csv and floors.csv files:
-            </label>
-            <div>
-              <input 
-                id="file-upload"
-                type="file"
-                name="file-teams"
-                accept=".csv"
-                onChange={(e) => setFile1(e.target.files[0])}
-              />
+        <div class="main" id="section1">
+          <div className='topLine'>
+            <div className='titleContainer'>
+              <h1 className='title'>
+                <span>
+                  <a className = 'titleLink' href="index.html">Space-Probe</a>
+                  </span>
+              </h1>
             </div>
-            <div>
-              <input 
-                id="file-upload2"
-                type="file"
-                name="file-floors"
-                onChange={(e) => setFile2(e.target.files[0])}
-              />
+            <div className="navbar">
+              <nav>
+                <ul>
+                  <li className="aboutLink" href="#section2" onClick={handleClickScroll}>
+                    About
+                  </li>
+                  <li className='loginLink'> 
+                    Login
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <div className='inputContainer'>
+            <div className='box'>
+            <div className='Form'>
+              <label className='PromptLabel'>
+                Upload teams.csv and floors.csv files:
+              </label>
               <div>
-                <button className='button2' onClick={handleSubmission}>Submit</button>
+                <input 
+                  id="file-upload"
+                  type="file"
+                  name="file-teams"
+                  accept=".csv"
+                  onChange={(e) => setFile1(e.target.files[0])}
+                />
+              </div>
+              <div>
+                <input 
+                  id="file-upload2"
+                  type="file"
+                  name="file-floors"
+                  onChange={(e) => setFile2(e.target.files[0])}
+                />
+                <div>
+                  <button className='button2' onClick={handleSubmission}>Submit</button>
+                </div>
               </div>
             </div>
+            </div>
           </div>
-          </div>
+        </div>
+        <div className='infoContainer' id='section2'>
+          <h2>
+            About Space-Probe
+          </h2>
+          <p>
+            This will contain the information about our API and functionality.
+          </p>
         </div>
       </header>
     </div>
